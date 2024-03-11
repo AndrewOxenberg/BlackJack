d5074139de8375c6589010fdac0819c7111048c4
Anrew Oxenberg <adoxenberg@gmail.com>
2024-03-11 00:11:02 -0400

    continued table.css need to do navbar and adjustable widths

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 
 import './styles/components/login.css';
+import './styles/components/table.css';
 import './styles/styles.css';
 
+
+console.log('Hello World!');
+
+document.querySelector('.hamburger_button').addEventListener('click', function() {
+    document.querySelector('.navbar_container').classList.toggle('active');
+});
