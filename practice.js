9693a22a20b9b83f07d338aeec2d4ca11352d8c7
Anrew Oxenberg <adoxenberg@gmail.com>
2024-03-12 17:12:43 -0400

    finished menu, started popup, need to finish js and css for popup and organize css

---
+++ practice.js
@@ -0,0 +1,46 @@
+
+// import './styles/styles.css';
+// import './styles/components/table.css';
+// import './styles/components/menu.css';
+// import './styles/components/login.css';
+// import './styles/components/signup.css';
+
+
+// let headingElement = document.querySelector('.heading');;
+// headingElement.innerText = "Not Todo";
+
+
+
+
+// let nameText = document.getElementById('nameText');
+// let nameInput = document.getElementById('nameInput');
+// let submitName = document.getElementById('submitName');
+
+
+// function changeName() {
+//     nameText.innerText = nameInput.value;
+//     localStorage.setItem('name', nameInput.value);
+// }
+
+
+// submitName.addEventListener('click', changeName);
+
+// nameText.innerText = localStorage.getItem('name');
+
+
+
+
+
+// let input = document.getElementById('input');
+// let submitButton = document.getElementById('submit');
+
+// function addTodo() {
+//     let todo = input.value;
+//     let p = document.createElement('p');
+//     p.innerText = todo;
+//     document.getElementById('items').appendChild(p);
+
+
+// }
+
+// submitButton.addEventListener('click', addTodo);
--- src/main.js
+++ src/main.js
@@ -1,11 +1,90 @@
 
-import './styles/components/login.css';
-import './styles/components/table.css';
-import './styles/styles.css';
+let settings = document.querySelector('.hamburger_button')
+let navbar = document.querySelector('.navbar_container')
+let overlay = document.querySelector('.overlay')
+let imgOverlays = document.querySelectorAll('img')
+let decision_button = document.querySelectorAll('.decision_button')
+let popupTrigger = document.querySelector('.table_difficulty_button');
+
+let difficultySelectorEasy = document.querySelector('.dif_tab_button_easy');
+let difficultySelectorMedium = document.querySelector('.dif_tab_button_medium');
+let difficultySelectorHard = document.querySelector('.dif_tab_button_hard');
+
+settings.addEventListener('click', () => {
+
+    navbar.classList.toggle('show_navbar')
+    overlay.classList.toggle('active')
+    imgOverlays.forEach(img => img.classList.toggle('active')); // Toggle for each image
+    decision_button.forEach(decision_button => decision_button.classList.toggle('active')); // Toggle for each image
+    popupTrigger.classList.toggle('active')
 
 
-console.log('Hello World!');
 
-document.querySelector('.hamburger_button').addEventListener('click', function() {
-    document.querySelector('.navbar_container').classList.toggle('active');
+  
+})
+
+
+//popup
+
+let popup = document.querySelector('.difficulty_popup');
+
+//open popup from difficulty button
+popupTrigger.addEventListener('click', () => {
+
+    if (popup.style.display === 'none'|| !overlay.classList.contains('active')) {
+        toggleBackground()
+    }
+    popup.style.display = 'block'
+    openTab('easy')
+})
+
+//close popup from X button
+let closeButton = document.querySelector('.close_button')
+closeButton.addEventListener('click', () => {
+
+
+    if (popup.style.display === 'block' || overlay.classList.contains('active')) {
+        toggleBackground()
+    }
+    popup.style.display = 'none'
+
+
+})
+
+
+function openTab(tabName) {
+
+    var i, tabcontent, tablinks
+
+    // Hide all tabs
+    tabcontent = document.getElementsByClassName('tab');
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none'
+    }
+
+    // Show the selected tab
+    document.getElementById(tabName).style.display = 'block'
+}
+
+
+
+difficultySelectorEasy.addEventListener('click', openTab('easy'));;
+
+difficultySelectorMedium.addEventListener('click', () => {
+  openTab('medium');
 });
+
+difficultySelectorHard.addEventListener('click', () => {
+  openTab('hard');
+});
+
+function toggleBackground() {
+    overlay.classList.toggle('active')
+    imgOverlays.forEach(img => img.classList.toggle('active')); // Toggle for each image
+    decision_button.forEach(decision_button => decision_button.classList.toggle('active')); // Toggle for each image
+    popupTrigger.classList.toggle('active')
+}
+
+
+
